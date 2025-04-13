f6686fd806f9c5b3f08c0ed667d1d146b8eeefca
abramsymons <abramsymons@gmail.com>
2025-04-13 11:31:13 +0330

    release 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zellular",
-    version="0.1.7",
+    version="0.1.8",
     author="Mahdi",
     author_email="mahdi@zellular.xyz",
     description="SDK package for zelluar sequencer",
