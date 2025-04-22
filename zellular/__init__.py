a2fdf9b66afa524bdd895ddd66ff6b20c7bd144d
abramsymons <abramsymons@gmail.com>
2025-04-22 09:04:16 +0330

    add Zellular, StaticNetwork, EigenlayerNetwork to root of zellular package

---
+++ zellular/__init__.py
@@ -0,0 +1,5 @@
+from .zellular import Zellular
+from .networks.static import StaticNetwork
+from .networks.eigenlayer import EigenlayerNetwork
+
+__all__ = ["Zellular", "StaticNetwork", "EigenlayerNetwork"]
