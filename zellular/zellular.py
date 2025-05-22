f8107ff62b24fcfabdf291bfe01b47d70eea0980
abramsymons <abramsymons@gmail.com>
2025-05-22 16:08:00 +0330

    raise original errors

--- zellular/zellular.py
+++ zellular/zellular.py
@@ -59,11 +59,7 @@ class Zellular:
     def get_last_finalized(self) -> dict[str, Any]:
         url = f"{self.gateway}/node/{self.app}/batches/finalized/last"
         response = requests.get(url, timeout=self.timeout)
-        if response.status_code != 200:
-            raise ConnectionError(
-                f"Failed to get last finalized batch: status code {response.status_code}, "
-                f"{response.text}"
-            )
+        response.raise_for_status()
 
         result = response.json()
         if result["status"] != "success":
@@ -94,8 +90,7 @@ class Zellular:
 
         url = f"{self.gateway}/node/{self.app}/batches"
         response = requests.put(url, data=batch, headers={"Content-Type": "text/plain"}, timeout=self.timeout)
-        if response.status_code != 200:
-            raise ConnectionError(f"Failed to send batch: {response.text}")
+        response.raise_for_status()
 
         if not blocking:
             return None
@@ -178,10 +173,7 @@ class Zellular:
                 f"{self.gateway}/node/{self.app}/batches/finalized?after={index}",
                 timeout=self.timeout,
             )
-            if response.status_code != 200:
-                raise ConnectionError(
-                    f"Failed to get finalized batches: {response.text}"
-                )
+            response.raise_for_status()
 
             data = response.json()["data"]
             if not data:
